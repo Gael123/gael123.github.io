cf6e27941854d7d310a532e515c07270e17ee087
Gael Jerop <gael.jerop@yahoo.com>
2019-10-15 14:36:35 +1100

    theme working

--- _plugins/hex_to_rgb.rb
+++ _plugins/hex_to_rgb.rb
@@ -17,16 +17,16 @@
 # Returns an array of decimal values for the parse hex characters
 #
 
-module Jekyll
-  module HexToRGB
-    def hex_to_rgb(hexval)
-      if hexval.length.even?
-        hexval.scan(/../).map {|color| color.to_i(16)}
-      else
-        hexval.scan(/./).map {|color| (color+color).to_i(16)}
-      end
-    end
-  end
-end
+# module Jekyll
+#   module HexToRGB
+#     def hex_to_rgb(hexval)
+#       if hexval.length.even?
+#         hexval.scan(/../).map {|color| color.to_i(16)}
+#       else
+#         hexval.scan(/./).map {|color| (color+color).to_i(16)}
+#       end
+#     end
+#   end
+# end
 
-Liquid::Template.register_filter(Jekyll::HexToRGB)
+# Liquid::Template.register_filter(Jekyll::HexToRGB)
